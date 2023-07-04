92cde0b2e69bf0344e53d440e195d37fede5703f
Jaroslaw Chajneta <chajnetajaroslaw@gmail.com>
2023-07-04 20:34:16 +0200

    first commit

---
+++ randomtrivia.mjs
@@ -0,0 +1,35 @@
+import random from "random";
+import fs from "fs";
+
+//generate random number and log to console
+const trivias = "trivia.json";
+let parsedTrivias;
+
+function randomTrivia() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(trivias, (err, data) => {
+            if (err) {
+                console.error('Error reading file', err);
+                reject(err);
+                return;
+            }
+
+            parsedTrivias = JSON.parse(data);
+
+            let randomTrivia = random.int(0, parsedTrivias.trivia.length);
+
+            resolve(parsedTrivias.trivia[randomTrivia]);
+        });
+    });
+}
+
+// randomTrivia()
+//     .then((trivia) => {
+//         console.log(trivia);
+//     })
+//     .catch((err) => {
+//         console.error('Error retrieving random trivia', err);
+//     });
+
+
+export de
---
+++ server.mjs
@@ -0,0 +1,31 @@
+'use strict';
+
+import randomTrivia from './randomtrivia.mjs';
+
+import express from 'express';
+
+const port = 3000
+
+const app = express();
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+
+    randomTrivia().then((trivia) => {
+        res.render('page', {
+            title: 'Hello World',
+            question: trivia.question,
+            answer: trivia.answer
+            
+
+        })
+    })
+    
+})
+
+app.listen(port, () => {
+    console.log('Example app listening on port 3000! 🧑‍🚀')
+    //give clickable link 
+    console.log(`http://localhost:${port}`)
+})
