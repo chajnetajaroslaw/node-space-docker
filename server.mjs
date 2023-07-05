b39492471317b6bd8051f66c2b882b601f459fc3
Jaroslaw Chajneta <chajnetajaroslaw@gmail.com>
2023-07-05 16:46:05 +0200

    stats added

--- server.mjs
+++ server.mjs
@@ -1,5 +1,7 @@
 'use strict';
 
+// setup // // // // // // // 
+
 import randomTrivia from './randomtrivia.mjs';
 
 import express from 'express';
@@ -8,14 +10,25 @@ const port = 3000
 
 const app = express();
 
+// stats // // // // // //   
+
+let hits = 0;
+
+
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
 
+    const uptimeInSeconds = process.uptime();
+    const formattedUptime = new Date(uptimeInSeconds * 1000).toISOString().substr(11, 8);
+
     randomTrivia().then((trivia) => {
+        hits += 1
         res.render('page', {
             question: trivia.question,
-            answer: trivia.answer
+            answer: trivia.answer,
+            hits: hits,
+            uptime: formattedUptime
 
 
         })
